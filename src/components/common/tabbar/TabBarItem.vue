5a39d4df8da520b07441ac71407bcee1fb5daa43
yuebinzhuang <1641030405@qq.com>
2020-11-01 16:48:23 +0800

    新增了一些内容

--- src/components/common/tabbar/TabBarItem.vue
+++ src/components/common/tabbar/TabBarItem.vue
@@ -1,51 +1,63 @@
 <template>
-  <div id="tab-bar-item" @click="itemClick">
-    <div class="item-icon" v-show="!isActive"><slot name="icon"></slot></div>
-    <div class="item-active-icon" v-show="isActive"><slot name="active-icon"></slot></div>
-    <div class="item-text" :style="activeStyle"><slot name="text"></slot></div>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :style="activeStyle">
+      <slot name="item-text"></slot>
+    </div>
   </div>
 </template>
 
 <script>
-	export default {
-		name: "TabBarItem",
-    props: {
-			link: {
-				type: String,
-        required: true
-      }
-    },
-    computed: {
-			isActive() {
-				return this.$route.path.indexOf(this.link) !== -1
-      },
-      activeStyle() {
-				return this.isActive ? {'color': 'red'} : {}
-      }
-    },
-    methods: {
-			itemClick() {
-				this.$router.replace(this.link)
-      }
+export default {
+  name: "TabBarItem",
+  props: {
+   path: String,
+   activeColor: {
+     type: String,
+     default: 'red'
+   }
+  },
+  data() {
+    return {
+      // isActive: true,
     }
-	}
+  },
+
+  computed: {
+    isActive() {
+      //indexof !== -1 时为true,表示（this.path）== this.$route.path
+      return this.$route.path.indexOf(this.path) !== -1
+    },
+    activeStyle() {
+      return this.isActive ? {color: this.activeColor} : {}
+    }
+  },
+
+  methods: {
+    itemClick() {
+      this.$router.replace(this.path)
+    }
+  },
+}
 </script>
 
 <style scoped>
-  #tab-bar-item {
-    flex: 1;
-  }
+    .tab-bar-item{
+      flex: 1;
+      text-align: center;
+      height: 49px;
+      font-size: 14px;
+    }
 
-  .item-icon img, .item-active-icon img {
-    width: 24px;
-    height: 24px;
-    margin-top: 5px;
-    vertical-align: middle;
-  }
+    .tab-bar-item img{
+      width: 24px;
+      height: 24px;
+      margin-top: 3px;
+      vertical-align: middle;
+    }
 
-  .item-text {
-    font-size: 12px;
-    margin-top: 3px;
-    color: #333;
-  }
+    .active{
+      color: red;
+    }
 </style>
